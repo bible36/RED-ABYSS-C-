314169554b3d4a31eaafa057851aa4ea0bca4ad8
bible36 <46374100+bible36@users.noreply.github.com>
2019-06-02 01:48:27 -0700

    Update intro_to_game.bat

--- intro_to_game.bat
+++ intro_to_game.bat
@@ -2,3 +2,11 @@
 echo ______________________
 echo saving......
 pause;
+echo ______________________
+echo 1=keep going 2=exit
+set /p input=slect 1 or 2:
+if %input%==1 (
+goto 
+if %input%==2 (
+exit:
+)
